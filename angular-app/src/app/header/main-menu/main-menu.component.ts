6d16b889347d2512b816282959cc3f935525a122
yvesmanalo <23yvesmanalo@gmail.com>
2025-11-24 21:10:26 +0800

    Add top menu and main menu components and css styles

---
+++ angular-app/src/app/header/main-menu/main-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-menu',
+  imports: [],
+  templateUrl: './main-menu.component.html',
+  styleUrl: './main-menu.component.css',
+})
+export class MainMenuComponent {
+
+}
---
+++ angular-app/src/app/top-header/top-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'top-header',
+  imports: [],
+  templateUrl: './top-header.component.html',
+  styleUrl: './top-header.component.css',
+})
+export class TopHeaderComponent {
+
+}
