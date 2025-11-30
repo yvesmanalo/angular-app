7bd9bb1394283f5ea6f9cc41294042fbe717888c
yvesmanalo <23yvesmanalo@gmail.com>
2025-11-30 20:56:00 +0800

    feature-8: Create custom event binding to filter products

--- angular-app/src/app/container/product-list/filter/filter.component.ts
+++ angular-app/src/app/container/product-list/filter/filter.component.ts
@@ -1,19 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-filter',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.css',
 })
 export class FilterComponent {
   @Input()
-  all: number = 0
+  all: number = 0;
 
   @Input()
-  inStock: number = 0
+  inStock: number = 0;
 
   @Input()
-  outOfStock: number = 0
+  outOfStock: number = 0;
 
+  @Output()
+  selectedFilterRadioButtonChanged: EventEmitter<string> = new EventEmitter<string>();
+
+  selectedFilterRadioButton: string = 'all';
+
+  onSelectedFilterRadioButtonChanged() {
+    this.selectedFilterRadioButtonChanged.emit(this.selectedFilterRadioButton);
+  }
 }
