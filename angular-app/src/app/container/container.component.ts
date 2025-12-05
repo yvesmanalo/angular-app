e55bf80438aeb0911923ca1b88f37ccd471cfc14
yvesmanalo <23yvesmanalo@gmail.com>
2025-12-05 20:39:22 +0800

    [feature-11] - Add product detail

--- angular-app/src/app/container/container.component.ts
+++ angular-app/src/app/container/container.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { SearchComponent } from './search/search.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-container',
-  imports: [SearchComponent, ProductListComponent],
+  imports: [SearchComponent, ProductListComponent, ProductDetailComponent, CommonModule],
   templateUrl: './container.component.html',
   styleUrl: './container.component.css',
 })
