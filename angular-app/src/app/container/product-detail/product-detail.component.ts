43e8f8915ec2ab1659572b38883cf70f8704a5a7
yvesmanalo <23yvesmanalo@gmail.com>
2025-12-07 21:31:01 +0800

    update files for search, container and product detail component

--- angular-app/src/app/container/product-detail/product-detail.component.ts
+++ angular-app/src/app/container/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ProductListComponent } from '../product-list/product-list.component';
+import { Product } from '../../Models/Product';
 
 @Component({
   selector: 'product-detail',
@@ -7,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './product-detail.component.css',
 })
 export class ProductDetailComponent {
+  @Input() productListComp: ProductListComponent = undefined;
 
+  product: Product
+
+  ngOnInit(){
+    this.product = this.productListComp.selectedProduct;
+  }
 }
